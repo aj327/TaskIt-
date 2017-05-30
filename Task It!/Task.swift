41d5d457785fbc7c5014b880b70bdc31ef689955
Aaditya Jain <aaditya.jain327@gmail.com>
2017-05-30 19:11:50 -0400

    Task Class

---
+++ Task It!/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Task It!
+//
+//  Created by Aaditya Jain on 5/30/17.
+//  Copyright © 2017 Aaditya Jain. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var taskName = ""
+    var taskImportance = false
+}
